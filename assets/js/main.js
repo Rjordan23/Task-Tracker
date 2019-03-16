b3397551ec0a384b904aebf268079c0051561549
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2019-03-15 23:44:34 -0400

    created base file structure and logic

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Check off Task with a click
+$('ul').on('click', 'li', function() {
+  // Add line-through and color class
+  $(this).toggleClass('completed');
+});
+
+// X to delete Task
+$('ul').on('click', 'span', function(e) {
+  $(this).parent().fadeOut(500, function() {
+    // Removes parent element
+    $(this).remove();
+  });
+  // Stop Bubbling
+  e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e) {
+  if(e.which === 13) {
+    // Text entered into input
+    var taskText = $(this).val();
+    $(this).val('');
+    // Create new li and add to parent ul
+    $('ul').append('<li><span>X</span> ' + taskText + '</li>');
+  }
+});
